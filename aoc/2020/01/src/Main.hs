505faf25d030c16fbf60f5343d0e283c51b25fd2
Edmund Wu <fangkazuto@gmail.com>
2020-12-29 17:06:21 -0500

    2020: embed input.txt into executable

--- aoc/2020/01/src/Main.hs
+++ aoc/2020/01/src/Main.hs
@@ -1,9 +1,14 @@
-module Main
-( main ) where
-  main :: IO ()
-  main = do
-    fileLines <- map (read :: String -> Int) . lines <$> readFile "input.txt" -- <$> = fmap (Functor map)
-    -- Part 1
-    print . head $ [ a * b | a <- fileLines, b <- fileLines, a + b == 2020 ]
-    -- Part 2
-    print . head $ [ a * b * c | a <- fileLines, b <- fileLines, c <- fileLines, a + b + c == 2020 ]
+{-# LANGUAGE TemplateHaskell #-}
+
+import EmbedIO
+
+main :: IO ()
+main = do
+  -- <$> is fmap
+  -- if using readFile, then it returns IO String
+  -- in which case fmap allows lines to operate on the String of the IO String
+  let fileLines = map (read :: String -> Int) . lines $ $(embedFile "2020/01/input.txt")
+  -- Part 1
+  print . head $ [ a * b | a <- fileLines, b <- fileLines, a + b == 2020 ]
+  -- Part 2
+  print . head $ [ a * b * c | a <- fileLines, b <- fileLines, c <- fileLines, a + b + c == 2020 ]
--- aoc/2020/02/src/Main.hs
+++ aoc/2020/02/src/Main.hs
@@ -1,37 +1,38 @@
-module Main
-( main ) where
+{-# LANGUAGE TemplateHaskell #-}
 
-  data Rule = Rule
-    { range_min :: Int
-    , range_max :: Int
-    , target :: Char
-    , pwd :: String }
-    deriving (Show)
+import EmbedIO
 
-  parseLine :: String -> Rule
-  parseLine rule = Rule l' r' target pwd
-    where
-      parts = words rule
-      (l, r) = break (== '-') (parts !! 0)
-      l' = (read :: String -> Int) l
-      r' = (read :: String -> Int) $ tail r
-      target = head $ parts !! 1
-      pwd = parts !! 2
+data Rule = Rule
+  { range_min :: Int
+  , range_max :: Int
+  , target :: Char
+  , pwd :: String }
+  deriving (Show)
 
-  filter' :: Rule -> Bool
-  filter' (Rule range_min range_max target pwd) = len >= range_min && len <= range_max
-    where
-      len = length $ filter (== target) pwd
+parseLine :: String -> Rule
+parseLine rule = Rule l' r' target pwd
+  where
+    parts = words rule
+    (l, r) = break (== '-') (parts !! 0)
+    l' = (read :: String -> Int) l
+    r' = (read :: String -> Int) $ tail r
+    target = head $ parts !! 1
+    pwd = parts !! 2
 
-  filter'' :: Rule -> Bool
-  filter'' (Rule l' r' target pwd) = (tl == target || tr == target) && tl /= tr
-    where
-      tl = pwd !! (l' - 1)
-      tr = pwd !! (r' - 1)
+filter' :: Rule -> Bool
+filter' (Rule range_min range_max target pwd) = len >= range_min && len <= range_max
+  where
+    len = length $ filter (== target) pwd
 
-  main :: IO ()
-  main = do
-    parsed <- map parseLine . lines <$> readFile "input.txt"
-    print . length $ filter filter' parsed  -- Part One
-    print . length $ filter filter'' parsed -- Part Two
+filter'' :: Rule -> Bool
+filter'' (Rule l' r' target pwd) = (tl == target || tr == target) && tl /= tr
+  where
+    tl = pwd !! (l' - 1)
+    tr = pwd !! (r' - 1)
+
+main :: IO ()
+main = do
+  let parsed = map parseLine . lines $ $(embedFile "2020/02/input.txt")
+  print . length $ filter filter' parsed  -- Part One
+  print . length $ filter filter'' parsed -- Part Two
 
