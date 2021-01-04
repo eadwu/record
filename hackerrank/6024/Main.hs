cfc84d0bc156925c740fe5d2f182c99afd4e80f6
Edmund Wu <fangkazuto@gmail.com>
2021-01-04 18:23:52 -0500

    hackerrank: include solution for 6024

---
+++ hackerrank/6024/Main.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE FlexibleInstances, UndecidableInstances, DuplicateRecordFields #-}
+
+module Main where
+
+import Control.Monad
+import Data.Array
+import Data.Bits
+import Data.List
+import Data.List.Split
+import Data.Set
+import Debug.Trace
+import System.Environment
+import System.IO
+import System.IO.Unsafe
+
+-- Complete the angryProfessor function below.
+angryProfessor k a
+  | late < k  = "YES"
+  | otherwise = "NO"
+  where late = length $ Data.List.filter (> 0) a
+
+readMultipleLinesAsStringArray :: Int -> IO [String]
+readMultipleLinesAsStringArray 0 = return []
+readMultipleLinesAsStringArray n = do
+    line <- getLine
+    rest <- readMultipleLinesAsStringArray(n - 1)
+    return (line : rest)
+
+main :: IO()
+main = do
+    stdout <- getEnv "OUTPUT_PATH"
+    fptr <- openFile stdout WriteMode
+
+    t <- readLn :: IO Int
+
+    forM_ [1..t] $ \t_itr -> do
+        nkTemp <- getLine
+        let nk = words nkTemp
+
+        let n = read (nk !! 0) :: Int
+
+        let k = read (nk !! 1) :: Int
+
+        aTemp <- getLine
+
+        let a = Data.List.map (read :: String -> Int) . words $ aTemp
+
+        let result = angryProfessor k a
+
+        hPutStrLn fptr result
+
+    hFlush fptr
+    hClose fptr
